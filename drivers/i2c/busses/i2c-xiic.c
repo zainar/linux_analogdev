0e946f7cce97a74074a34132e1bc0607bd99b468
Jeremy McNicoll <jeremy.mcnicoll@zainartech.com>
2022-06-05 02:01:05 +0000

    Add IRQ number in handler

--- drivers/i2c/busses/i2c-xiic.c
+++ drivers/i2c/busses/i2c-xiic.c
@@ -975,7 +975,7 @@ static irqreturn_t xiic_isr(int irq, void *dev_id)
 	if (irq != 30)
 		return ret;
 
-	dev_dbg(i2c->adap.dev.parent, "%s entry\n", __func__);
+	dev_dbg(i2c->adap.dev.parent, "%s entry irq=%d \n", __func__, irq);
 
 	isr = xiic_getreg32(i2c, XIIC_IISR_OFFSET);
 	ier = xiic_getreg32(i2c, XIIC_IIER_OFFSET);
